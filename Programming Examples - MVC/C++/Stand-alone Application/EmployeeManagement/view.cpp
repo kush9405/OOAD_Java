7f93a6c945411056311da8ed2d74628837e2991e
Kushagra Agarwal <kushagraagarwal2003@gmail.com>
2025-04-19 16:33:31 +0530

    Remove unused MVC components and example files
    - Deleted view.cpp and view.h files from C++ Employee Management example.
    - Removed Java Employee MVC components including Employee.java, EmployeeController.java, EmployeeView.java, and related files.
    - Cleaned up unnecessary .DS_Store files from Java projects.
    - Eliminated Java Web Application files related to Employee Management System.
    - Removed various Java classes and example files demonstrating inheritance, interfaces, and other concepts.
    - Deleted README and other documentation files from UNIT_3.

--- Programming Examples - MVC/C++/Stand-alone Application/EmployeeManagement/view.cpp
+++
@@ -1,8 +0,0 @@
-// view.cpp
-#include "view.h"
-
-void EmployeeView::displayEmployeeDetails(const Employee &emp) {
-    std::cout << "Employee ID: " << emp.getId() << std::endl;
-    std::cout << "Name: " << emp.getName() << std::endl;
-    std::cout << "Salary: $" << emp.getSalary() << std::endl;
-}
--- box.java
+++
@@ -1,9 +0,0 @@
-public class box {
-    double height;
-    double width;
-    double depth;
-    
-    void display() {
-        System.out.println("Height: " + height + " Width: " + width + " Depth: " + depth);
-    }
-}
